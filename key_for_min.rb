5fe7de46b49f7d825927aeed06e009738e3540d8
David Yu <david.yu@yale.edu>
2020-05-20 17:16:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   current_min = Float::INFINITY
   name_hash.each do |key, value|
-    if value < current_min do
+    if value < current_min
       min_key = key 
       current_min = value
     end
